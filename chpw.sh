b3d5ff6acf9f86d2ac4fa04ea9fdc5a873c4dec5
Vic Xu <837458581@qq.com>
2016-11-22 01:06:05 -0500

    chpw

--- chpw.sh
+++ chpw.sh
@@ -2,11 +2,14 @@
 BASEDIR=chpw.tmp
 IDPWFILE=idpw.txt
 SERVERLIST=list.txt
+USER=`who am i | awk '{print $1}'`
 mkdir ./${BASEDIR}
 echo "please input id:passwd ;example user:aa12#zdad"
 read IDPW
 echo $IDPW > ./${BASEDIR}/${IDPWFILE}
 echo "please input server list "
+#touch ./${BASEDIR}/${SERVERLIST}
+> ./${BASEDIR}/${SERVERLIST}
 vim ./${BASEDIR}/${SERVERLIST}
 if [ -f ./${BASEDIR}/${IDPWFILE} ] ;then
 ID=$(cat ./${BASEDIR}/${IDPWFILE} |awk -F ':' '{print $1}')
@@ -25,3 +28,11 @@ else
 echo "error : server list doesn't exist "
 exit
 fi
+
+for i in `cat ./${BASEDIR}/${SERVERLIST}` 
+do 
+scp ./${BASEDIR}/${IDPWFILE} ${USER}@${i}:/tmp
+ssh ${USER}@${i} -t "sudo chpasswd < /tmp/${IDPWFILE}"
+ssh ${USER}@${i} -t "sudo rm -f /tmp/${IDPWFILE}"
+done
+
